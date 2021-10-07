e7462d42db77e51c37d6307acc19c04c57d91f93
Ahuacate <43461046+ahuacate@users.noreply.github.com>
2021-10-07 12:11:34 +0700

    Delete pve_test.sh

--- scripts/pve_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-COMMON_PVE_SOURCE="${DIR}/../common/pve/source"
-COMMON_BASH_SOURCE="${DIR}/../common/bash/source"
-
-echo ${DIR}
-echo ${COMMON_PVE_SOURCE}
-echo ${COMMON_BAS_SOURCE}
