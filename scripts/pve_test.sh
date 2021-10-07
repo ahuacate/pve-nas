07d1a9fa642a0afbeb4273a65794f82e1588ab38
Ahuacate <43461046+ahuacate@users.noreply.github.com>
2021-10-07 12:08:57 +0700

    Create pve_test.sh

---
+++ scripts/pve_test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+COMMON_PVE_SOURCE="${DIR}/../common/pve/source"
+COMMON_BASH_SOURCE="${DIR}/../common/bash/source"
+
+echo ${DIR}
+echo ${COMMON_PVE_SOURCE}
+echo ${COMMON_BAS_SOURCE}
